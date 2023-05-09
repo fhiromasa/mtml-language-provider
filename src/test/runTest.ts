aae229393167033dea08a03fb0db632ca24dbcd1
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2023-05-09 12:09:32 +0900

    add test initial code

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,6 +1,6 @@
 import * as path from "path";
 
-import { runTests } from "@vscode/test-electron";
+import { runTests, downloadAndUnzipVSCode } from "@vscode/test-electron";
 
 async function main() {
 	try {
@@ -12,9 +12,17 @@ async function main() {
 		// Passed to --extensionTestsPath
 		const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
+		const testWorkspace = path.resolve(__dirname, "../../testFixture");
+		const vscodeExecutablePath = await downloadAndUnzipVSCode("1.67.0");
+
 		// Download VS Code, unzip it and run the integration test
 		console.log("test start");
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
+		await runTests({
+			vscodeExecutablePath,
+			extensionDevelopmentPath,
+			extensionTestsPath,
+			launchArgs: [testWorkspace, "--disable-extensions"],
+		});
 	} catch (err) {
 		console.error("Failed to run tests");
 		process.exit(1);

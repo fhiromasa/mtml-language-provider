7d6f2c1e2afa679dfaa38b66794969c9e317f91b
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2023-07-19 15:47:00 +0900

    hoverProviderのmarkdown text生成をmarkdownモジュールに移動

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,7 @@ export * as Setting from "./utilities/setting";
 export * as Codegen from "./utilities/codegen";
 export * as Data from "./utilities/data";
 export * as Variable from "./utilities/variable";
+export * as Markdown from "./utilities/markdown";
 
 // 正規表現だと改行を取れないらしい
 export const tagRegex = new RegExp(/<\$?mt(app)?:?[^>]+/i);
